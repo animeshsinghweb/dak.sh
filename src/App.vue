f14e721c718f374825d5c1ffad9ee9fd8bd44fa8
Daksh Miglani <hello@dak.sh>
2017-07-02 17:16:37 +0530

    Made Home Page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <v-app>
     <v-navigation-drawer
       persistent
-      :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
     >
@@ -10,7 +9,7 @@
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
-        >
+          @click="openLink(item.link)">
           <v-list-tile value="true">
             <v-list-tile-action>
               <v-icon light v-html="item.icon"></v-icon>
@@ -24,70 +23,19 @@
     </v-navigation-drawer>
     <v-toolbar>
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        light
-        @click.native.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.native.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.native.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title v-text="title" class="beWhite"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        light
-        @click.native.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <main>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
             <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
+            <router-view></router-view>
           </v-layout>
         </v-slide-y-transition>
       </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-item>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed">
       <span>&copy; 2017</span>
     </v-footer>
@@ -102,17 +50,29 @@
         drawer: true,
         fixed: false,
         items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
+          { icon: 'home', title: 'Home', link: '/' },
+          { icon: 'info', title: 'About', link: '/about' },
+          { icon: 'bubble_chart', title: 'Blog', link: 'https://blog.dak.sh' },
+          { icon: 'gavel', title: 'Work', link: '/work' },
+          { icon: 'widgets', title: 'Download CV', link: '/assets/daksh_cv.pdf' }
         ],
-        miniVariant: false,
         right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Daksh Miglani'
       }
+    },
+    methods: {
+        openLink: function(link) {
+          window.location = link
+        }
     }
   }
 </script>
 
 <style lang="stylus">
   @import './stylus/main'
+  img
+    border-radius 50%;
+    width 17%;
+  .beWhite
+    color #fff !important
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+  <h2>Hello, I'm Daksh.</h2>
+  <div class="v2">
+    <p>I'm a 15 Year Old Wizard, Who uses Python-JavaScript and C++ for Magic. I'm also a Deep-Learning Student and a Space Enthusaist.</p>
+  </div>
+  <div class="v3">
+     <v-icon medium fa dark class="ico" v-for="(item, i) in items" :key="i" @click="openLink(item.url)">{{item.name}}</v-icon>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      items: [
+        {
+          name: 'github',
+          url: 'https://github.com/DakshMiglani'
+        },
+        {
+          name: 'facebook',
+          url: 'https://facebook.com/DakshMTGV'
+        },
+        {
+          name: 'twitter',
+          url: 'https://twitter.com/Dak_ssh'
+        },
+        {
+          name: 'instagram',
+          url: 'https://instagram.com/DakshMiglani'
+        },
+        {
+          name: 'medium',
+          url: 'https://medium.com/@dak.sh'
+        },
+      ]
+    }
+  },
+  methods: {
+    openLink: (l) => {
+      window.open(l, '_blank')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  h2 {
+    color: #263238 !important;
+    font-size: 50px;
+    text-align: center;
+  }
+  p {
+    text-align: center;
+    font-size: 20px;
+  }
+  .v2 {
+    width: 50vw;
+  }
+  @media (max-width: 576px) {
+    .v2 {
+      width: 90vw;
+    }
+  }
+  .ico {
+     margin-right:5%;
+  }
+  .v3 {
+    text-align: center;
+    align-items: center;
+  }
+</style>
