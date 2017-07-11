a35ae6c88773d1a50648512e5052c178e82c5d9f
Daksh Miglani <hello@dak.sh>
2017-07-11 15:51:29 +0530

    updated bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,8 +2,40 @@
   <div>
   <h2>About, Me.</h2>
   <div class="v2">
-<p>Hi, I'm Daksh; I am a 15 year old, who loves to program. I was 8 when I got my first computer, and certainly I was lucky to be one from those families who can afford a Computer in those old times especially in India, People mostly live in poverty here.</p> <p>Leaving that aside, I was fascinated by computers and how the components of the computers worked. I wanted to know more about them, I spent most of my daily time on it. If I Speak in Layman's terms, I loved Computers and I always wanted to do something related to them, So I started Programming when I was 10.</p> <p>My Parents weren't happy due to that, They tried to shape me into a sportsperson and tried separating me from the family by sending me to Boarding school, But you know I loved computers. So It didn't matter, I came back and enjoyed it.</p> <p>After that, the phase of my life changed. I learnt new things and started making money out of it and my parents were actually impressed and convinced that I could do something in life. So they started being supportive and to be honest, my dad was always supporting me, I don't know about my mom, you know typical Indian Mom's.</p> <p>As the phase changed, I became more interested in Science (especially cosmology) as well. To be honest, the only subject I ever loved except computer science was Science, Cause you get to know about yourself and how the world works in the real way. So I became a Science Fnatic, Made some tools to support my love for Cosmos.</p> <p>Now, I didn't expect this to happen; But now I find myself interested in machine learning and AI which is a field of Computer Science and requires prior knowledge of advance math equations and science, Hence I Chose Physics, Chemistry, Mathematics and Computer Science as my Subjects in my current grade (11th)</p> <p>I would like to end here, by saying just a single sentence. <br>
-        "Whatever you do in life, Love it and Enjoy working on it because at the end of the day, You did what you loved and there's no complaining."</p>
+    <p>
+      Hello There, I'm Daksh. A 15 (16 on 13th July 2017) Year Old, who loves to program. I actually didn't know what to do since I was a kid and I never matched with people nearby with me. I mean People will always be calling me outside to play with them, I usually didn't go with them and When you stop ignoring people, They start bullying you and I was bullied my entire life.
+    </p>
+    <p>
+      My Dad was always a grat guy being supportive to me and he got me my first computer when I was a small-little kid, and when you get something new you get fairly excited and the excitement doesn't stay much longer but for me that wasn't the case, to this day I love computers and I get excited when I see computers lying around my room.
+    </p>
+    <p>
+      I Started Programming in the early days and by that time I didn't know how all these things work and I could actually do something from it. So I started writing code in the most basic languages for kids such as <a href="https://scratch.mit.edu" target="_blank">Scratch</a>, <a href="https://en.wikipedia.org/wiki/QBasic" target="_blank">QBasic</a> and <a href="https://turtleacademy.com" target="_blank">The Logo (Turtle Language)</a>. While these languages didn't really allow me to build amazing things that I build to this day but they gave me a ground of what computers can do and what they can be made to do.
+    </p>
+    <p>
+      My Mom and Dad didn't like that either, they prefered taking the hard-way and got me into a boarding school for a few months and about the love for technology, it never stopped me and I eventually came back and started writing code and
+      One day I was paid to code which really Impressed my parents and they got a the idea of what I was doing and they found out that at this age I'm doing the right thing that usually others don't do.
+      </p>
+    <p>
+      I wasn't really interested in school from the begining and never became the "topper" as you school guys may call it but to me actually grades do not matter, skills do because you can mug-up the things and get good grades but you won't be able to mug-up and build a skill sooner or later. I loved Science to be honest and that was the only subject except computer science that I had my legs over.
+    </p>
+    <p>
+      Then one day, I found out about <a href="https://www.python.org" target="_blank">Python</a> and Damn! That was the best day of my life, Cause that's the language which had some grounds and I could easily program things with it whether they be web-apps (dynamic) or just simple desktop apps but Python had a lot of potential because It could be used anywhere and It was just a hell of a perfect Language and I loved it the most and Actually Python Got me into Web-Development.
+    </p>
+    <p>
+      I had some bad experiences then and was bullied a lot by some big YouTubers, I won't disclose the names either but they bullied me all together and It just felt like Why am I even alive and they did it in a way that got me into hard time, call it depression but sooner or later I found out <a href="https://nodejs.org" target="_blank">Node.js</a>, this thing was the future of Web-Tech and I got myself into it.
+    </p>
+    <p>
+      With Node.js making applications was so damn easy and in a User-Friendly way I was able to build some cool things and that allowed me to get into loving node.js for a year and then one day I heard about build mobile apps and desktop apps with Node.js and that told me that I chose the right stack and Now I was able to build Cross-Platform Apps Easily. I built some really powerful tools that actually helped people do some cool stuff.     
+    </p>
+    <p>
+      Node.js esentially helped me do all the stuff what I wanted to do and I was really happy and I was to be honest always possessive about cosmology and Artificial Intelligence and Sci-Fi films engaged those things in me and One day I decided that I will build Something for Artificial Intelligence and I found that AI is a field of Machine Learning that is called Reinforcement Learning, I started learning about ML. 
+    </p>
+    <p>
+      I started with a really handy library called <a href="http://scikit-learn.org/stable/index.html" target="_blank">Sci-Kit Learn</a> by Google, that allowed you to do most of the machine learning except Neural Networks and for neural networks I found google in 2015 was building something called <a href="https://www.tensorflow.org/" target="_blank">"Tensorflow"</a> and I started trying Tensorflow in the late 2016 and It actually came out to be really handy and built some really cool neural network algorithms such as Classifiers, Predicters, Computer Vision, etc and well that's where I was headed and started improving myself and I'm still getting there and I will eventually get there by working harder.
+    </p>
+    <p>
+      I Didn't expect this to happen that quickly but it did and the truth is time flies when you're working hard, I am in 11th Grade and I chose Physics, Chemistry, Computer Science and Mathematics as my primary subjects and I actually love all of them. I became a Full Stack Developer over a period of almost 6 years and now it feels like I made for it and my advice for every starter would always be Do whatever you love in life, It always pays well.
+    </p>
   </div>
   </div>
 </template>
